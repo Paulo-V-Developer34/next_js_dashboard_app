fd17d5fb410667ef503fa0bc60347eb83dafb127
Paulo-V-Developer34 <paulov2007nv@gmail.com>
2025-08-19 13:18:48 -0300

    add server-side validation

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -9,21 +9,47 @@ import { redirect } from 'next/navigation'
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'})
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string()
-})
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  date: z.string(),
+});
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 const CreateInvoice = FormSchema.omit({id: true, date: true})
 
-export async function createInvoice(formData: FormData){
-    const {customerId, amount, status} = CreateInvoice.parse({
+export async function createInvoice(prevState: State,formData: FormData){//adicionando prevState
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     })
+
+    //tratando os erros
+    if(!validatedFields.success){
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing fields. Failed to create invoice!'
+      }
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
+
     const amountInCents = amount * 100 //é uma boa prática mantermos valores monetásrios em centavos para evitarmos problemas de imprecisão de valores flutuantes
     const date = new Date().toISOString().split('T')[0] //estamos criando os dados que ainda não foram inseridos pelo usuário pois podem ser colocados automaticamente pelo sistema
 
@@ -41,12 +67,22 @@ export async function createInvoice(formData: FormData){
 
 const UpdateInvoice = FormSchema.omit({id: true, date: true})
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  //tratando os erros
+    if(!validatedFields.success){
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing fields. Failed to create invoice!'
+      }
+    }
+
+  const { customerId, amount, status } = validatedFields.data
  
   const amountInCents = amount * 100;
  
